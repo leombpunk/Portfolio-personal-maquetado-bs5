b8cbc1e305e04af34f29b27758749c6598f12886
Leo Boos <leomb_punk@hotmail.com>
2022-05-25 14:59:25 -0300

    primer commit
    incluye los archivos de testeos que he hecho de algunas giladas en CSS

---
+++ assets/js/indexJS.js
@@ -0,0 +1,4 @@
+document.getElementById("btnLogin").addEventListener('click', function(e) {
+    e.preventDefault();
+    window.location.href = "login.html";
+});
---
+++ assets/js/loginJS.js
@@ -0,0 +1,5 @@
+
+// document.getElementById('loginForm').addEventListener('submit', function(e){
+//     e.preventDefault();
+//     alert("apretaste el pinshi boton");
+// });
